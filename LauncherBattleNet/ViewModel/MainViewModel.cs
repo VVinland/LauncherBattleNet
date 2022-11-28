49e813a69adf89831cc37fc47a8335ffecd76d8e
Даниил <Vinland0_0@mail.ru>
2022-11-28 16:04:32 +0500

    Добавьте файлы проекта.

---
+++ LauncherBattleNet/ViewModel/MainViewModel.cs
@@ -0,0 +1,89 @@
+﻿using GalaSoft.MvvmLight.Command;
+using LauncherBattleNet.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace LauncherBattleNet.ViewModel
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        private Page Wow = new WowPage();
+        private Page HS = new HsPage();
+        private Page OverW = new OverWPage();
+        private Page HOTS = new HOTSPage();
+        private Page D3 = new Diablo3();
+        private Page SC = new SC();
+        private Page SC2 = new SCII();
+        private Page Wow3 = new Wow3Page();
+        private Page _CurPage = new WowPage();
+
+        public Page CurPage
+        {
+            get => _CurPage;
+            set => Set(ref _CurPage, value);
+        }
+
+        public ICommand OpenHSPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = HS);
+            }
+        }
+        public ICommand OpenWowPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage =Wow);
+            }
+        }
+        public ICommand OpenOverWPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = OverW);
+            }
+        }
+        public ICommand OpenHOTSWPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = HOTS);
+            }
+        }
+        public ICommand OpenD3WPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = D3);
+            }
+        }
+        public ICommand OpenSCPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = SC);
+            }
+        }
+        public ICommand OpenSC2WPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = SC2);
+            }
+        }
+        public ICommand OpenWow3Page
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = Wow3);
+            }
+        }
+    }
+
+}
---
+++ LauncherBattleNet/ViewModel/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace LauncherBattleNet.ViewModel
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void onPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName= null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            onPropertyChanged(PropertyName);
+            return true;
+        }
+            
+    }
+}
